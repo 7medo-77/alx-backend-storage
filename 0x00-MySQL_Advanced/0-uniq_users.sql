194bcb074d7c9623118c25e232be51ce777e509f
hmedo <hmedo@7medo>
2024-08-08 20:10:46 +0300

    0-uniq_users.sql_20-10

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- SQL script that creates a new table called users
+-- with constraints for id an email columns
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email STRING NOT NULL UNIQUE,
