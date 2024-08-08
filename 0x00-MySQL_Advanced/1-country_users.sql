b29c93daf163a9cfccbe8a187f44c6edf4590406
hmedo <hmedo@7medo>
2024-08-08 20:19:22 +0300

    1-country_users.sql_20-19

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- SQL script that creates a new table called users
--- with constraints for id an email columns
+-- with constraints for id, email, and country columns
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL,
 )
