e4d68c4817ee025749f82dadac1a7035a3d7f025
hmedo <hmedo@7medo>
2024-08-09 02:03:49 +0300

    3-glam_rock.sql_02-03

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- SQL script lists all the origins and fans of meta bands
 SELECT band_name,
 	-- (IF(split=NULL, 2022, split) - formed) AS lifespan
-	(IFNULL(split, 2020) - formed) AS lifespan
+	(IFNULL(split, 2022) - formed) AS lifespan
 	-- (split - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
