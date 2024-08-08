5651ad7996bbdfeb5946602734ee0bb761a271a0
hmedo <hmedo@7medo>
2024-08-09 00:57:18 +0300

    2-fans.sql_00-57

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,7 @@
 -- SQL script lists all the origins and fans of meta bands
+USE holberton;
 SELECT origin AS origin,
 	SUM(fans) AS nb_fans
-FROM holberton.metal_bands
+FROM metal_bands
 GROUP BY `origin`
 ORDER BY `nb_fans` DESC;
