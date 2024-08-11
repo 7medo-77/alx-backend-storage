586f511a73b4f0555d830dda34fae0b682144d32
hmedo <hmedo@7medo>
2024-08-11 19:29:09 +0300

    7-average_score.sql_19-29

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,36 @@
--- Creates a stored procedure ComputeAverageScoreForUser that
--- computes and stores the average score for a student.
+-- SQL stored procedure
+
+-- DELIMITER $$
+--
+-- DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+-- CREATE PROCEDURE ComputeAverageScoreForUser(
+--     user_id INT
+-- )
+-- BEGIN
+--     DECLARE total_score INT DEFAULT 0;
+--     DECLARE user_count INT DEFAULT 0;
+--     DECLARE avg_score FLOAT DEFAULT 0;
+--
+--     SELECT SUM(score)
+--         INTO total_score
+--         FROM corrections
+--         WHERE user_id = user_id;
+--
+--     SELECT COUNT(*)
+--         INTO user_count
+--         FROM corrections
+--         WHERE corrections.user_id = user_id;
+--
+--     SET avg_score = total_score / user_count;
+--
+--     UPDATE users
+--         SET average_score = IF(avg_score = 0, 0, avg_score)
+--         WHERE id = user_id;
+--
+-- END $$
+-- DELIMITER ;
+
+
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
