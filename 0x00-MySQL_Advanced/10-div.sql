d271d01d2bc2b2f0598575d0f0f20bcaf536dc84
hmedo <hmedo@7medo>
2024-08-12 01:24:30 +0300

    10-div.sql_01-24

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT
 BEGIN
-    DECLARE result DEFAULT 0;
+    DECLARE result FLOAT DEFAULT 0;
 
     SET result = IF(a = 0 OR b = 0, 0, a / b);
     RETURN result;
