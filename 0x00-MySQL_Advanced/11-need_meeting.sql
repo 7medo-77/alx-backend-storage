9a02156ffd55556c306ca4c481aa8db8c8526ca9
hmedo <hmedo@7medo>
2024-08-13 02:37:56 +0300

    11-need_meeting.sql_02-37

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,6 +1,7 @@
 -- SQL script that creates a view need_meeting
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name
     FROM students
-    WHERE score < 80 AND
-    ( last_meeting = NULL OR MONTH(DATEDIFF(CURDATE(), last_meeting)) > 1 );
+    WHERE (score < 80) AND
+    ( last_meeting IS NULL OR MONTH(DATEDIFF(CURDATE(), last_meeting)) > 1 );
