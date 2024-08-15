fc3ea38b4991a8508a3e2d5328c8ff492344cf08
hmedo <hmedo@7medo>
2024-08-15 03:52:02 +0300

    exercise.py_03-52

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,7 @@ Module which uses redis
 """
 import redis
 import uuid
+from typing import Union, Any, Callable
 
 
 class Cache:
@@ -12,7 +13,7 @@ class Cache:
     def __init__(self):
         self._redis.flushdb()
 
-    def store(self, data):
-        randomKey = str(uuid.uuid4())
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        randomKey: str = str(uuid.uuid4())
         self._redis.set(randomKey, data)
         return randomKey
