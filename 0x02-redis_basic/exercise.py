8bd524629ff44dbd1f2983a0fe7a8988f70af193
hmedo <hmedo@7medo>
2024-08-15 03:54:25 +0300

    exercise.py_03-54

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,9 +11,15 @@ class Cache:
     _redis = redis.Redis()
 
     def __init__(self):
+        """
+        Initializing an instance of Cache
+        """
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         randomKey: str = str(uuid.uuid4())
+        """
+        Store method which stores a new data type
+        """
         self._redis.set(randomKey, data)
         return randomKey
